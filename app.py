f94759eabe36d54f8dce9cba305e62b8a9669457
Shadow <shadow012.code@gmail.com>
2025-01-04 10:55:27 +0530

    Created Routes/Endpoints

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ client = MongoClient('localhost', 27017)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def Home():
+def Index():
     return render_template("index.html")
 
+@app.route('/Home', methods=['GET', 'POST'])
+def Home():
+    return "Hello World!"
+
+@app.route('/Report', methods=['GET', 'POST'])
+def Report():
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
